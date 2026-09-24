2d2f8fbe628867078a6b00d568b91a27015ade06
Vinushika M <vinudevi200@gmail.com>
2026-09-25 00:23:45 +0530

    Update 977-SquareOfaSortedArray.java

--- Two_Pointer/977-SquareOfaSortedArray.java
+++ Two_Pointer/977-SquareOfaSortedArray.java
@@ -1,10 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i =0;i<nums.length;i++){
-             nums[i]=(int)Math.pow(nums[i],2);
+        int left=0;
+        int right=nums.length-1;
+        int result[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            if(Math.abs(nums[left])< Math.abs(nums[right])){
+                result[i]=(int)Math.pow(nums[right],2);
+                right--;
+            }
+   
+            else{
+                result[i]=(int)Math.pow(nums[left],2);
+                left++;
+            }
         }
-        Arrays.sort(nums);
-        return nums;
+        return result;
     }
 }
